dbfe0858b46f62767f664e27c5535bd38eb7ad75
murki <markymeetsinternet@gmail.com>
2025-03-28 16:42:28 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/loserwhocodes/wgt@latest/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/loserwhocodes/wgt@master/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
