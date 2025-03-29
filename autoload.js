204d70ded5b0307910f55fc71d0d82ce21570a58
akainu00s <markandradeyu@gmail.com>
2025-03-29 10:12:56 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/loserwhocodes/wgt@main/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/loserwhocodes/wgt@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -26,19 +26,19 @@ function loadExternalResource(url, type) {
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
-	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-	]).then(() => {
-		// 配置选项的具体用法见 README.md
-		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "quit"]
-		});
-	});
+Promise.all([
+    loadExternalResource(live2d_path + "waifu.css", "css"),
+    loadExternalResource(live2d_path + "live2d.min.js", "js"),
+    loadExternalResource(live2d_path + "waifu-tips.js", "js")
+]).then(() => {
+    loadExternalResource(live2d_path + "src/index.js", "js").then(() => {
+        initWidget({
+            waifuPath: live2d_path + "waifu-tips.json",
+            cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+            tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "quit"]
+        });
+    });
+});
 }
 
 console.log(`
